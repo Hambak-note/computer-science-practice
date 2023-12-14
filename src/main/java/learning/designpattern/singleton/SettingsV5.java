5d5b2fb801a4f853b6dbbf94192f1a94c8e32645
Hambak-note <dudghks5722@gmail.com>
2023-12-14 17:00:02 +0900

    refactor(SettingsV5): 누락된 괄호 추가

--- src/main/java/learning/designpattern/singleton/SettingsV5.java
+++ src/main/java/learning/designpattern/singleton/SettingsV5.java
@@ -7,6 +7,7 @@ public class SettingsV5 {
     private static class SettingsHolder {
         private static final SettingsV5 SETTINGS_V_5 = new SettingsV5();
 
+    }
 
     public static SettingsV5 getInstance() {
        return SettingsHolder.SETTINGS_V_5;
