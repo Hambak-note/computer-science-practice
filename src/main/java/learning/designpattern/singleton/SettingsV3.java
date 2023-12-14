ccce4915ad10febfc6e821a131d33cfc16c8f376
Hambak-note <dudghks5722@gmail.com>
2023-12-14 16:28:35 +0900

    feat(SettingsV3): 이른 초기화를 사용한 싱글톤 구현
    동기화 과부화 문제를 해결하기 위해 이른 초기화를 사용한 싱글톤 구현
    그러나 이 방법은 인스턴스 자체가 무거울 시 생성에 많은 비용이 소모되는 단점이 있음

---
+++ src/main/java/learning/designpattern/singleton/SettingsV3.java
@@ -0,0 +1,13 @@
+package learning.designpattern.singleton;
+
+public class SettingsV3 {
+
+    private static final SettingsV3 INSTANCE = new SettingsV3();
+
+    private SettingsV3() {}
+
+    public static SettingsV3 getInstance() {
+
+       return INSTANCE;
+    }
+}
