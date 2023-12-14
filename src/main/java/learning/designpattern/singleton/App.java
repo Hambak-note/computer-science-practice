18e36cbe32cb343d733e2be0a01a7f4919b69c85
Hambak-note <dudghks5722@gmail.com>
2023-12-14 16:55:11 +0900

    feat(App) 리플렉션을 사용하여 싱글톤을 깨트리는 방법

--- src/main/java/learning/designpattern/singleton/App.java
+++ src/main/java/learning/designpattern/singleton/App.java
@@ -1,4 +1,18 @@
 package learning.designpattern.singleton;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
 public class App {
+
+    public static void main(String[] args) throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+
+        SettingsV5 settingsV5 = SettingsV5.getInstance();
+
+        Constructor<SettingsV5> constructor = SettingsV5.class.getDeclaredConstructor();
+        constructor.setAccessible(true);
+        SettingsV5 settingsV5Copy = constructor.newInstance();
+
+        System.out.println(settingsV5Copy == settingsV5);
+    }
 }
