7d61451161b3366164c9c2ae74f178ec91fc89bd
Hambak-note <dudghks5722@gmail.com>
2023-12-14 16:02:56 +0900

    init: 프로젝트 초기 세팅

---
+++ src/test/java/learning/designpattern/DesignpatternApplicationTests.java
@@ -0,0 +1,13 @@
+package learning.designpattern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignpatternApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
