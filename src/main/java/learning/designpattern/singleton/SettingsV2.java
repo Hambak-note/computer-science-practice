1f8124803836a11da9fdcd01932faaf02b64646b
Hambak-note <dudghks5722@gmail.com>
2023-12-14 16:21:22 +0900

    feat(SettingsV2): 동기화를 사용해 멀티쓰레드 safe하게 생성하는 방법
    그러나 위 방법은 동기화를 보장하기 위한 lock을 설정하는 과정에서 성능상 단점이 발생한다

---
+++ src/main/java/learning/designpattern/singleton/SettingsV2.java
@@ -0,0 +1,15 @@
+package learning.designpattern.singleton;
+
+public class SettingsV2 {
+
+    private static SettingsV2 instance;
+
+    private SettingsV2() {}
+
+    public static synchronized SettingsV2 getInstance() {
+
+        if(instance == null) instance = new SettingsV2();
+
+        return instance;
+    }
+}
